a890ccab7cb96c23bc3a67e44339ae258dd320fb
Wa Wang <callmewa@gmail.com>
2014-07-30 20:50:55 -0700

    added all necessary unit tests TODO: need to fix async instructions

--- test/feedTest.js
+++ test/feedTest.js
@@ -8,14 +8,42 @@ var assert = require("assert");
 
 describe('FeedTest', function(){
   describe('AddPost', function(){
-    it('should add the post without error', function(done){
+    it('drop current database', function(){
+      feed.client.flushdb();
+    });
+
+    it('should add and get followers without error', function(done){
+      feed.followUser('user1', 'user2');
+      feed.followUser('user1', 'user3');
+      feed.getFollowers('user1', function(err, follows){
+        assert(follows.indexOf('user2')!=-1 && follows.indexOf('user3')!=-1, 'missing follower(s)');
+      });
+      done();
+    });
+
+
+    it('should add and get the post without error', function(done){
       feed.addPost(feed.UserPost.createPost('post1', 'user1', 'text', 'blah blah blah'));
       feed.getPost('post1', function(err, post){
         assert.equal(JSON.parse(post).userId, 'user1');
-
-        console.log(post);
       });
       done();
-    })
+    });
+
+    //TODO: this need to be async or promised
+    it('should publish a post to feed without error', function(done){
+      feed.publishPostToFeeds('post1', 'user1', function(err, result){
+        console.log('published feed ' + result);
+      });
+      feed.getFeed('user2');
+      feed.getFeed('user3', function(err, feeds){
+        console.log(feeds);
+        done();
+      });
+
+    });
+
+
+
   })
 });
